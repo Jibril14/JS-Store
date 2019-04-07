a360f7ff6d8cb46df229210d5949552108a63876
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2019-04-07 08:32:19 +0100

    Connect Multiple Class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,4 @@
 class Product {
-    // title = 'DEFAULT';
-    // imageUrl;
-    // description;
-    // price;
-
     constructor(title, image, desc, price) {
         this.title = title;
         this.imageUrl = image;
@@ -12,8 +7,30 @@ class Product {
     }
 }
 
-const productList = {
-    products: [
+class singleProduct {
+    constructor(product, elemclass) {
+        this.product = product;
+    }
+    render() {
+        const prodElement = document.createElement("li");
+        prodElement.className = "product-item";
+        prodElement.innerHTML = `
+        <div>
+          <img style="maximum-width:600px; height:250px" src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+        return prodElement;
+    }
+}
+
+class ProductList {
+    products = [
         new Product(
             "A Laptop",
             "file:///C:/Users/ONIGA/Downloads/Product%20images/hpenv.jpg",
@@ -26,29 +43,22 @@ const productList = {
             "A carpet which you might like - or not.",
             89.99
         )
-    ],
+    ];
+    constructor() {}
+
     render() {
         const renderHook = document.getElementById("app");
         const prodList = document.createElement("ul");
         prodList.className = "product-list";
+
         for (const prod of this.products) {
-            const prodEl = document.createElement("li");
-            prodEl.className = "product-item";
-            prodEl.innerHTML = `
-        <div>
-          <img style="maximum-width:600px; height:250px" src="${prod.imageUrl}" alt="${prod.title}" >
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
+            const singleproduct = new singleProduct(prod);
+            const prodEl = singleproduct.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
     }
-};
+}
 
-productList.render();
+const productL = new ProductList();
+productL.render();
