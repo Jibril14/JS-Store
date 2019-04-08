55d630d4a6319afd83810f927c518aaf1c8b1770
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2019-04-08 14:50:06 +0100

    Add Shopping Cart

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,13 +7,48 @@ class Product {
     }
 }
 
+class ShoppingCart {
+    items = [];
+
+    set cartItems(value) {
+        this.items = value;
+        this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+            2
+        )}</h2>`;
+    }
+
+    get totalAmount() {
+        const sum = this.items.reduce((prevVal, curItem) => {
+            return prevVal + curItem.price;
+        }, 0);
+        return sum;
+    }
+
+    addProduct(product) {
+        const updatedItems = [...this.items];
+        updatedItems.push(product);
+        this.cartItems = updatedItems;
+    }
+    render() {
+        console.log("Aprod", this.items);
+        const cartElement = document.createElement("section");
+        cartElement.innerHTML = `
+            <h2>Total: \$${0}</h2>
+            <button>Order Now!</button>
+        `;
+        cartElement.className = "cart";
+        this.totalOutput = cartElement.querySelector("h2"); // Another property of this class
+        return cartElement;
+    }
+}
+
 class singleProduct {
     constructor(product) {
         this.product = product;
     }
 
     addToCart() {
-        console.log("Adding to cart");
+        App.addProductToCart(this.product); // App is a static method created down below
         console.log(this.product);
     }
     render() {
@@ -21,15 +56,18 @@ class singleProduct {
         prodElement.className = "product-item";
         prodElement.innerHTML = `
         <div>
+        
           <img style="maximum-width:600px; height:250px" src="${this.product.imageUrl}" alt="${this.product.title}" >
           <div class="product-item__content">
             <h2>${this.product.title}</h2>
             <h3>\$${this.product.price}</h3>
             <p>${this.product.description}</p>
             <button>Add to Cart</button>
+            
           </div>
         </div>
       `;
+
         const addToCartBtn = prodElement.querySelector("button");
         addToCartBtn.addEventListener("click", this.addToCart.bind(this));
         return prodElement;
@@ -41,20 +79,19 @@ class ProductList {
         new Product(
             "A Laptop",
             "file:///C:/Users/ONIGA/Downloads/Product%20images/hpenv.jpg",
-            "A soft pillow!",
+            "Hp laptop 16 gig ram",
             19.99
         ),
         new Product(
             "An Earbud",
             "file:///C:/Users/ONIGA/Downloads/Product%20images/earbud.jpg",
-            "A carpet which you might like - or not.",
+            "Listen to the Amazing sound of life.",
             89.99
         )
     ];
     constructor() {}
 
     render() {
-        const renderHook = document.getElementById("app");
         const prodList = document.createElement("ul");
         prodList.className = "product-list";
 
@@ -63,9 +100,37 @@ class ProductList {
             const prodEl = singleproduct.render();
             prodList.append(prodEl);
         }
-        renderHook.append(prodList);
+        return prodList;
     }
 }
 
-const productL = new ProductList();
-productL.render();
+class Shop {
+    render() {
+        const rootHook = document.getElementById("app");
+
+        // const cart = new ShoppingCart();
+        this.cart = new ShoppingCart(); // Now a property of shop obj
+        const cartElement = this.cart.render();
+        const productL = new ProductList();
+        const productListEl = productL.render();
+
+        rootHook.append(cartElement);
+        rootHook.append(productListEl);
+    }
+}
+
+class App {
+    static init() {
+        const shop = new Shop();
+        shop.render();
+        this.cart = shop.cart; // ie Shop.cart
+    }
+
+    static addProductToCart(product) {
+        const shopIt = new ShoppingCart();
+        shopIt.render();
+        this.cart.addProduct(product);
+    }
+}
+
+App.init();
